a54a97f1807f06d2fc0dee28b6001d3386ee6120
ngTamAn111103 <ng.t.ann2003@gmail.com>
2023-05-05 07:22:42 +0700

    Trang voucher của admin: Thêm xoá sửa, hiển thị danh sách, tìm kiếm Trang voucher của user: Tìm kiếm

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VoucherController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// trang của user
+Route::prefix('/user')->group(function () {
+    // voucher hiển thị giao diện, không truy vấn
+    Route::get('/voucher', function () {
+        return view('user.voucher');
+    });
+
+    // Trang xử lý: truy vấn code voucher user
+    Route::post('/voucher', [VoucherController::class, 'findVoucherUser'])->name('find-voucher-user');
+});
+
+
+// admin 
+Route::prefix('admin')->group(function () {
+    // trang hiển thị mặc định tất cả voucher
+    Route::get('/voucher', [VoucherController::class, 'index'])->name('voucher-index');
+    // trang trả ra kết quả tìm kiếm
+    Route::post('/voucher', [VoucherController::class, 'findVoucherAdmin'])->name('admin-voucher-timkiem');
+
+    // Thêm voucher
+    Route::post('/addvoucher', [VoucherController::class, 'store'])->name('them-voucher');
+    // Xoá voucher
+    Route::post('/deletevoucher/{id_voucher}', [VoucherController::class, 'destroy'])->name('xoa-voucher');
+    // Sửa voucher
+    Route::post('/editvoucher/{id_voucher}', [VoucherController::class, 'update'])->name('sua-voucher');
 });
