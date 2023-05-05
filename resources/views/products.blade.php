31a70b8cbc75390b1405622ddcf7bfb2ef857515
Taigg <21211tt4977@mail.tdc.edu.vn>
2023-05-05 09:36:22 +0700

    MOi them hinh

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -6,10 +6,11 @@
     @foreach($products as $product)
         <div class="col-xs-18 col-sm-6 col-md-3">
             <div class="thumbnail">
-                <img src="{{ $product->image }}" alt="">
+            <img src="{{asset('images/'.$product->image)}}" width="100" height="100" class="img-responsive" alt="">
                 <div class="caption">
                     <h4>{{ $product->name }}</h4>
                     <p>{{ $product->description }}</p>
+                    
                     <p><strong>Price: </strong> {{ $product->price }}$</p>
                     <p class="btn-holder"><a href="{{ route('add.to.cart', $product->id) }}" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
                 </div>
